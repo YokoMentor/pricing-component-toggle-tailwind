32b15c41419f70d1d2674aeabc21d3623f8535b6
YokoMentor <91747328+YokoMentor@users.noreply.github.com>
2024-08-25 23:37:06 +0300

    Added page sections

--- src/App.jsx
+++ src/App.jsx
@@ -14,12 +14,19 @@ function App() {
           </div>
       </div>
       <div className='flex flex-col justify-center items-center'>
-        <div className='flex flex-col justify-center items-center bg-white rounded-xl h-[452px]'>
+        <div className='flex flex-col justify-center items-center bg-white rounded-xl h-[452px] px-10'>
           <h2 className='text-primary-text text-lg mb-6'>Basic</h2>
-          <div className='flex flex-row justify-center items-center text-price'>
+          <div className='flex flex-row justify-center items-center text-price mb-8'>
             <div className='text-[40px] mr-1'>$</div>
             <div className='text-7xl tracking-tight'>199.99</div>
           </div>
+          <hr className="solid"></hr>
+          <div className='my-3.5 text-primary-text text-[15px]'>500 GB Storage</div>
+          <hr className="solid"></hr>
+          <div className='my-3.5 text-primary-text text-[15px]'>2 Users Allowed</div>
+          <hr className="solid"></hr>
+          <div className='my-3.5 text-primary-text text-[15px]'>Send up to 3 GB</div>
+          <hr className="solid"></hr>
         </div>
       </div>
     </div>
