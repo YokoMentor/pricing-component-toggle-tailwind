27d7bdf9bf1d7589d21a82404dec18ae2ef7a373
YokoMentor <91747328+YokoMentor@users.noreply.github.com>
2024-08-25 22:15:29 +0300

    Created initial toggle switch

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,17 @@ import './App.css'
 
 function App() {
   return (
-    <div className='flex justify-center font-sans bg-bg'>Hello</div>
+    <div className='flex min-h-screen justify-center items-center bg-bg'>
+      <div className='flex flex-col justify-center items-center'>
+          <h1 className='text-3xl'>Our Pricing</h1>
+          <div className='flex flex-row items-center justify-center'>
+            <div className='text-text text-[12px] mr-4'>Annually</div>
+            <input type="checkbox" id="toggle-switch" hidden/>
+            <label htmlFor="toggle-switch" className="switch hover:cursor-pointer md:hover:opacity-50"></label>
+            <div className='text-text text-[12px] ml-2.5 mr-2'>Monthly</div>
+          </div>
+      </div>
+    </div>
   )
 }
 
