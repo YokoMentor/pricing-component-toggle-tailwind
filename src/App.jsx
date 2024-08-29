d1029a965f75e1842d19ef82c0eef84ad46109fc
YokoMentor <91747328+YokoMentor@users.noreply.github.com>
2024-08-29 23:21:25 +0300

    Toggle functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './App.css'
 
 function App() {
+
+  const[annualBasic, setAnnualBasic] = useState(199.99);
+  const[annualProfessional, setAnnualProfessional] = useState(249.99);
+  const[annualMaster, setAnnualMaster] = useState(399.99);
+  const[toggle, setToggle] = useState(false);
+
+  function handleClick(e){
+    if (toggle) {
+      setAnnualBasic(199.99);
+      setAnnualProfessional(249.99);
+      setAnnualMaster(399.99);
+    } else {
+      setAnnualBasic(19.99);
+      setAnnualProfessional(24.99);
+      setAnnualMaster(39.99);
+    }
+    setToggle(!toggle);
+  }
+
+
   return (
     <div className='flex flex-col min-h-screen justify-center items-center bg-bg bg-bg-mobile bg-no-repeat bg-[194px_-36px] md:bg-bg-desktop md:bg-desktop'>
       <div className='flex flex-col justify-center items-center mb-20 md:mb-8 mt-[60px] md:mt-[68px]'>
@@ -9,7 +29,7 @@ function App() {
           <div className='flex flex-row items-center justify-center'>
             <div className='text-toggle-label text-[15px] mr-6'>Annually</div>
             <input type="checkbox" id="toggle-switch" hidden/>
-            <label htmlFor="toggle-switch" className="switch hover:cursor-pointer md:hover:opacity-50"></label>
+            <label htmlFor="toggle-switch" className="switch hover:cursor-pointer md:hover:opacity-50" onClick={handleClick}></label>
             <div className='text-toggle-label text-[15px] ml-6'>Monthly</div>
           </div>
       </div>
@@ -18,7 +38,7 @@ function App() {
           <h2 className='text-primary-text text-lg mb-6'>Basic</h2>
           <div className='flex flex-row justify-center items-center text-price mb-8'>
             <div className='text-[40px] mr-1'>$</div>
-            <div className='text-7xl tracking-tight'>199.99</div>
+            <div className='text-7xl tracking-tight'>{annualBasic}</div>
           </div>
           <hr className="solid"></hr>
           <div className='my-3.5 text-primary-text text-[15px]'>500 GB Storage</div>
@@ -33,7 +53,7 @@ function App() {
           <h2 className='text-white text-lg mb-6'>Professional</h2>
           <div className='flex flex-row justify-center items-center text-white mb-8'>
             <div className='text-[40px] mr-1'>$</div>
-            <div className='text-7xl tracking-tight'>249.99</div>
+            <div className='text-7xl tracking-tight'>{annualProfessional}</div>
           </div>
           <hr className="solid"></hr>
           <div className='my-3.5 text-white text-[15px]'>1 TB Storage</div>
@@ -48,7 +68,7 @@ function App() {
           <h2 className='text-primary-text text-lg mb-6'>Master</h2>
           <div className='flex flex-row justify-center items-center text-price mb-8'>
             <div className='text-[40px] mr-1'>$</div>
-            <div className='text-7xl tracking-tight'>399.99</div>
+            <div className='text-7xl tracking-tight'>{annualMaster}</div>
           </div>
           <hr className="solid"></hr>
           <div className='my-3.5 text-primary-text text-[15px]'>2 TB Storage</div>
